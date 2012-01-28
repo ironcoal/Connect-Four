d49a8cc7e0dca40f86f7337676b69704887a9544
Darius Fischer <irondwarf1@gmail.com>
2012-01-28 13:53:12 +0100

    (c)

--- tic.c
+++ tic.c
@@ -9,7 +9,7 @@ int main(void) {
     int i = 0;
     char input;
     system("cls");
-    puts("Welcome to TicTacToe 1.0! © DF");
+    puts("Welcome to TicTacToe 1.0! (c) DF");
     
     while (1) {
         puts("To start a new game, press \"n\", to "
